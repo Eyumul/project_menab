ac5bc2af3891499f8bae36c14a3ea128ac2f3091
Eyumul <eyuelmulugeta60@gmail.com>
2024-05-31 22:13:00 +0300

    movie cards created and dashboard, home & movies page are modified

---
+++ frontend/ABC_cinema/components/dropdownmenu.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <form>
+            <select class="w-[380px] h-[50px] border-none bg-black ring-[3px] ring-[rgb(0,137,208,0.5)] focus:ring-[rgb(0,137,208)] text-[24px] text-center  rounded-[20px]">
+                <option>No categories</option>
+                <option>Genre</option>
+                <option>Directors</option>
+            </select>
+        </form>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ frontend/ABC_cinema/pages/moviesFeed.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="h-full bg-black text-white pb-40">
+        <div class="flex justify-between items-end px-[138px] pt-[100px]">
+            <div class="flex space-x-8 justify-center pt-16 content end">
+                <img src="/public/figmaImage/movie.png"/>
+                <p class="s32 font-normal">Movies</p>
+            </div>
+            <div class="flex space-x-[34px] items-center">
+                <p class="text-[24px] font-light">Browse movies by</p>
+                <Dropdownmenu/>
+            </div>
+        </div>
+        <div class="my-[144px] flex space-x-[27px] justify-center">
+            <MovieCardOne/>
+            <MovieCardOne/>
+            <MovieCardOne/>
+            <MovieCardOne/>
+        </div>
+        <div class="my-[144px] flex space-x-[27px] justify-center">
+            <MovieCardOne/>
+            <MovieCardOne/>
+            <MovieCardOne/>
+            <MovieCardOne/>
+        </div>
+        <div class="my-[144px] flex space-x-[27px] justify-center">
+            <MovieCardOne/>
+            <MovieCardOne/>
+            <MovieCardOne/>
+            <MovieCardOne/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
